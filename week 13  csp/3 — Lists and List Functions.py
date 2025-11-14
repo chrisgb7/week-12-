04da66df9dce198fe99f2f02cad66ce74aa72515
chrisgb7 <gc6567824@gmail.com>
2025-11-14 20:48:43 +0000

    jjj

--- week 13  csp/3 — Lists and List Functions.py
+++ week 13  csp/3 — Lists and List Functions.py
@@ -14,18 +14,29 @@ print(type(my_list)) # <class 'list'>
 #this makes our job easier
 #when we need to manage multiple related items
 #performance task maker
-print(my_list)
-print(type(my_list))
-print(my_list[0]) #access first item
-print(my_list[1:4]) #access second item
-print(my_list[0:]) # [1, 2, 3, 4, 5]
-my_list.append(6) 
-print(my_list) # [1, 2, 3, 4, 5
-my_list.extend([10,11,12,13,14]) 
-#add 500 more numbers to the list
-my_list.extend(range(15,999))
-print(my_list)
+# print(my_list)
+# print(type(my_list))
+# print(my_list[0]) #access first item
+# print(my_list[1:4]) #access second item
+# print(my_list[0:]) # [1, 2, 3, 4, 5]
+# my_list.append(6) 
+# print(my_list) # [1, 2, 3, 4, 5
+# my_list.extend([10,11,12,13,14]) 
+# #add 500 more numbers to the list
+# my_list.extend(range(15,999))
+# print(my_list)
 # Examples:
+new_list = ['a', 'b', 'c']
+print(new_list)
+new_list.append('d')
+print(new_list)
+removed_item = new_list.pop(1)  #removes item at index 1
+print(removed_item)
+print(new_list)
+#sortinmg the list 
+numbers = [4, 2, 5, 1, 3]
+numbers.sort()
+print(numbers) # [1, 2, 3, 4, 5]
 
 my_list = ['apple', 'banana', 'cherry']
 print(my_list[0])         # apple
@@ -40,8 +51,10 @@ print(my_list)
 numbers = [3, 1, 4, 2]
 numbers.sort()
 print(numbers)
-
-
+#inserting an item at a specific positoin
+# 2 10
+numbers.insert(2, 101000)
+print (numbers)
 # Practice Problems:
 
 # Create a list with 5 of your favorite foods.
